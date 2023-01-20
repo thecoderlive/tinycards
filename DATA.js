043700af48248f1496e8e7307ea723876038be20
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-01-20 16:47:30 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,30 @@
+export const item = {
+    "id": "10a68ffd-4b74-4940-9fcf-1f9ec970f346",
+    "board_list": [
+        {
+            "id": "08f1b360-0084-4605-bcdf-5ee54512805f",
+            "logo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/172.png",
+            "about_card": "Win even or bank onto strategy impact trouble. Office wonder help without rule. Apply ability concern model."
+        },
+        {
+            "id": "719c8f75-3ac2-4948-b8d1-2ebde3c15c99",
+            "logo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/108.png",
+            "about_card": "Table whether wait painting majority once. Feeling they clearly home activity. Seem be consumer blue crime couple pattern."
+        },
+        {
+            "id": "34950cfc-1647-4132-875a-285cbc5312e0",
+            "logo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/358.png",
+            "about_card": "Animal nice population true if poor never."
+        },
+        {
+            "id": "36fb72ad-b4b9-49e8-b937-e6ea40b6ca14",
+            "logo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/326.png",
+            "about_card": "Card nothing real phone per. Choice above class certain. Road camera law door dog present purpose."
+        },
+        {
+            "id": "11e58d0e-185d-4d61-920f-a6a1a885654a",
+            "logo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/98.png",
+            "about_card": "Everybody send entire blood. Myself particularly school reveal allow."
+        }
+    ]
+}
